6ffca216bd35a2d7eab3e8ea6672da97b37cd826
Minh Thịnh <minhthinh.phng@gmail.com>
2023-06-14 15:27:18 +0700

    update code

--- main.go
+++ main.go
@@ -1,24 +1,9 @@
 package main
 
 import (
-	"learn_go/defer_panic_recover_return"
-	"learn_go/difference"
-	"learn_go/goroutine"
-	"time"
+	"learn_go/unbuffered"
 )
 
 func main() {
-	defer_panic_recover_return.Run()
-}
-
-func actionGoroutine() {
-	// ex
-	go goroutine.MakeASimpleCoffee()
-
-	/* Wait a cup of coffee is made*/
-	time.Sleep(time.Second * 150)
-}
-
-func actionDifference1() {
-	difference.Difference1()
+	unbuffered.Run()
 }
