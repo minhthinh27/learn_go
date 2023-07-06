8c4157225af2618d7659cf07e3170746438eb03f
Pham Nguyen Minh Thinh <minhthinh.phng@gmail.com>
2023-07-06 14:12:42 +0700

    Container With Most Water

--- main.go
+++ main.go
@@ -3,62 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	test := []int{2, 2, 2, 1, 1, 2}
-	sortColors(test)
-	// fmt.Println(sortColors(test))
+	test := []int{1, 8, 6, 2, 5, 4, 8, 3, 7}
+	//sortColors(test)
+	fmt.Println(maxArea(test))
 }
 
-// func sortColors(nums []int) {
-// 	mapColor := make(map[int]int, len(nums))
+func maxArea(height []int) int {
+	i, j := 0, len(height)-1
+	length := len(height) - 1
+	res := 0
 
-// 	for _, v := range nums {
-// 		mapColor[v]++
-// 	}
+	for i != j {
+		temp := 0
+		if height[i] < height[j] {
+			temp = height[i] * length
+			i++
+		} else {
+			temp = height[j] * length
+			j--
+		}
 
-// 	fmt.Println(mapColor)
+		length--
 
-// 	// red: 0 , white: 1 , blue: 2
-// 	nameColor := 0
-// 	i := 0
-// 	for i < len(nums) {
-// 		value, ok := mapColor[nameColor]
-// 		if !ok || value == 0 {
-// 			nameColor++
-// 		} else if value > 0 {
-// 			nums[i] = nameColor
-// 			mapColor[nameColor] = value - 1
-// 			i++
-// 		}
-// 	}
-
-// 	// for i, _ := range nums {
-// 	// 	if value, ok := mapColor[nameColor]; ok && value > 0 {
-// 	// 		mapColor[nameColor]--
-// 	// 		nums[i] = nameColor
-// 	// 	} else {
-// 	// 		nameColor += 1
-// 	// 	}
-// 	// }
-
-// 	fmt.Println(nums)
-// }
-
-func sortColors(nums []int) {
-	if len(nums) <= 1 {
-		return
-	}
-	var start, end = 0, len(nums) - 1
-	for i := 0; i <= end; i++ {
-		// swap current to end
-		if nums[i] == 2 {
-			nums[end], nums[i] = nums[i], nums[end]
-			end--
-			i--
-		} else if nums[i] == 0 {
-			nums[start], nums[i] = nums[i], nums[start]
-			start++
+		if temp > res {
+			res = temp
 		}
 	}
 
-	fmt.Println(nums)
+	return res
 }
