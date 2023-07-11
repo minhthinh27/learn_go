6cad7673c9d79f3c3851cc5444b8d364e549be8d
Pham Nguyen Minh Thinh <minhthinh.phng@gmail.com>
2023-07-11 22:16:19 +0700

    decodeMessage solution

--- main.go
+++ main.go
@@ -5,39 +5,46 @@ import (
 )
 
 func main() {
-	// var allowed = "abc"
-	// var words = []string{"a", "b", "c", "ab", "ac", "bc", "abc"}
-	var nums = []int{0, 1, 4, 6, 7, 10}
-	var diff = 3
-	fmt.Println(arithmeticTriplets(nums, diff))
+	var key = "the quick brown fox jumps over the lazy dog"
+	var message = "vkbs bs t suepuv"
+	// var nums = []int{0, 1, 4, 6, 7, 10}
+	// var diff = 3
+	fmt.Println(decodeMessage(key, message))
 }
 
-func arithmeticTriplets(nums []int, diff int) int {
-	result := 0
-	mapNums := make(map[int]int, len(nums))
+func decodeMessage(key string, message string) string {
+	mapLetters := make(map[rune]rune, 26)
+	result := []rune{}
 
-	for i, v := range nums {
-		mapNums[v] = i
-	}
-
-	i, j := 0, 1
-
-	for j < len(nums) {
-		if nums[j]-nums[i] == diff {
-			if _, ok := mapNums[diff+nums[j]]; ok {
-				result++
-			}
-			i++
-		}
-
-		if nums[j]-nums[i] > diff {
-			i++
-		}
-
-		if nums[j]-nums[i] < diff {
-			j++
+	var charater rune = 0
+	for _, v := range key {
+		if _, ok := mapLetters[v]; !ok && v != 32 {
+			mapLetters[v] = 'a' + charater
+			charater++
 		}
 	}
 
-	return result
+	for _, v := range message {
+		text := mapLetters[v]
+		if v == 32 {
+			result = append(result, 32)
+		} else {
+			result = append(result, text)
+		}
+	}
+
+	return string(result)
+	// table := map[rune]byte{' ': ' '}
+	// for _, c := range key {
+	// 	if _, ok := table[c]; !ok {
+	// 		table[c] = byte(len(table) - 1 + 'a')
+	// 	}
+	// }
+
+	// var buffer bytes.Buffer
+	// for _, c := range message {
+	// 	buffer.WriteByte(table[c])
+	// }
+
+	// return buffer.String()
 }
