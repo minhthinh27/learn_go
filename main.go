89353b251eed9184bc04564d00f64172ab813729
Pham Nguyen Minh Thinh <minhthinh.phng@gmail.com>
2023-07-13 15:59:47 +0700

    countPoints solution

--- main.go
+++ main.go
@@ -9,42 +9,29 @@ func main() {
 	// var message = "vkbs bs t suepuv"
 	// var nums = []int{0, 1, 4, 6, 7, 10}
 	// var diff = 3
-	var words = []string{"cd", "ac", "dc", "ca", "zz"}
-	fmt.Println(maximumNumberOfStringPairs(words))
+	var rings = "B0B6G0R6R0R6G9"
+	//var words = []string{"cd", "ac", "dc", "ca", "zz"}
+	fmt.Println(countPoints(rings))
 }
 
-func maximumNumberOfStringPairs(words []string) int {
-	mapWords := make(map[string]bool)
+func countPoints(rings string) int {
+	mapRings := make(map[string]map[string]bool)
 	result := 0
-	for _, str := range words {
-		temp := reverseString(str)
 
-		if _, ok := mapWords[temp]; !ok {
-			mapWords[str] = true
+	for i := 0; i < len(rings); i = i + 2 {
+		_, ok := mapRings[string(rings[i+1])]
+		if !ok {
+			mapRings[string(rings[i+1])] = make(map[string]bool)
+			mapRings[string(rings[i+1])][string(rings[i])] = false
 		} else {
-			result++
+			_, ok2 := mapRings[string(rings[i+1])][string(rings[i])]
+			if !ok2 {
+				mapRings[string(rings[i+1])][string(rings[i])] = false
+				if len(mapRings[string(rings[i+1])]) == 3 {
+					result++
+				}
+			}
 		}
 	}
-
 	return result
-	// res := 0
-	// h := make(map[string]bool)
-	// for _, w := range words {
-	// 	if r := fmt.Sprintf("%c%c", w[1], w[0]); h[r] {
-	// 		res++
-	// 	} else {
-	// 		h[w] = true
-	// 	}
-	// }
-	// return res
-}
-
-func reverseString(value string) string {
-	runes := []rune(value)
-
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
-	}
-
-	return string(runes)
 }
