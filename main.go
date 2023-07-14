4c94b07f7928491f2f71e43d834ea72c41b792c1
Pham Nguyen Minh Thinh <minhthinh.phng@gmail.com>
2023-07-14 23:12:38 +0700

    sortPeople solution

--- main.go
+++ main.go
@@ -9,29 +9,39 @@ func main() {
 	// var message = "vkbs bs t suepuv"
 	// var nums = []int{0, 1, 4, 6, 7, 10}
 	// var diff = 3
-	var rings = "B0B6G0R6R0R6G9"
-	//var words = []string{"cd", "ac", "dc", "ca", "zz"}
-	fmt.Println(countPoints(rings))
+	//var rings = "B0B6G0R6R0R6G9"
+	var names = []string{"Alice", "Bob", "Marry"}
+	var heights = []int{155, 185, 160}
+	fmt.Println(sortPeople(names, heights))
 }
 
-func countPoints(rings string) int {
-	mapRings := make(map[string]map[string]bool)
-	result := 0
+func sortPeople(names []string, heights []int) []string {
 
-	for i := 0; i < len(rings); i = i + 2 {
-		_, ok := mapRings[string(rings[i+1])]
-		if !ok {
-			mapRings[string(rings[i+1])] = make(map[string]bool)
-			mapRings[string(rings[i+1])][string(rings[i])] = false
-		} else {
-			_, ok2 := mapRings[string(rings[i+1])][string(rings[i])]
-			if !ok2 {
-				mapRings[string(rings[i+1])][string(rings[i])] = false
-				if len(mapRings[string(rings[i+1])]) == 3 {
-					result++
-				}
-			}
-		}
+	mapHeight := make(map[int]string)
+
+	for i, v := range heights {
+		mapHeight[v] = names[i]
 	}
+
+	start, end := 0, len(heights)-1
+
+	for start < end {
+		i := start
+
+		for i < end {
+			if heights[i+1] > heights[start] {
+				heights[start], heights[i+1] = heights[i+1], heights[start]
+			}
+			i++
+		}
+
+		start++
+	}
+
+	result := []string{}
+	for _, val := range heights {
+		result = append(result, mapHeight[val])
+	}
+
 	return result
 }
