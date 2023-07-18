9a0953af008f83adb88aa586ada63fbcd5dcf3f2
Pham Nguyen Minh Thinh <minhthinh.phng@gmail.com>
2023-07-18 13:59:49 +0700

    update code

---
+++ goroutine/example1.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func init() {
+	runtime.GOMAXPROCS(1)
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 3; i++ {
+			for j := 'a'; j < 'z'; j++ {
+				fmt.Printf("%c", j)
+			}
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 3; i++ {
+			for j := 'A'; j < 'Z'; j++ {
+				fmt.Printf("%c", j)
+			}
+		}
+	}()
+
+	wg.Wait()
+
+	fmt.Println("Completely")
+}
